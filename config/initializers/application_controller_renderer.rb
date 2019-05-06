6392574beac4c34fcaa5d7faedfdd1ea1d0f86f5
Ryota201902 <deraryo327@gmail.com>
2019-05-06 03:48:23 +0000

    投稿一覧ページ、投稿詳細ページを作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '0a00dff64b0dd96a67c73149463a25f596f0f055e30f00aeaeecf2d889bf2dcf576952d6ca4f41ed967417dc963b58539a84dd8d9b660eaf8af326f2db361721'
