138264ce740cc29fb7c91d3c2f83b9d900fb5b64
Ryota201902 <deraryo327@gmail.com>
2019-05-11 12:57:39 +0000

    イベント一覧ページのレイアウト、マイページでイベントに参加しているユーザーにトークルームへが表示されるようにusers_controllerを編集しました。コメントにプロフィール画像がつくようにしました

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -10,6 +10,11 @@ class ParticipatesController < ApplicationController
 		event = Event.find(params[:event_id])
 		participate = current_user.participates.find_by(event_id: event.id)
 		participate.destroy
-		redirect_back(fallback_location: events_path)
+		@path = Rails.application.routes.recognize_path(request.referer)
+		if @path[:controller] == "events"
+			redirect_to events_path
+		else
+			redirect_back(fallback_location: user_path(current_user))
+		end
 	end
 end
