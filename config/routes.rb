754dd970779380220bf9d668a48c3d46c8727212
Ryota201902 <deraryo327@gmail.com>
2019-05-12 14:21:28 +0000

    投稿イベントを編集できるようにしました

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'root#top'
-  resources :events, only:[:new, :create, :index, :show, :destroy] do
+  resources :events, only:[:new, :create, :index, :show, :edit, :update, :destroy] do
   	resource :post_comments, only: [:create, :destroy]
   	resource :participates, only: [:create, :destroy]
   end
