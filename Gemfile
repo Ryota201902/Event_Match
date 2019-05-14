c8455b58b260145a85b2067811545f98fc2283b8
Ryota201902 <deraryo327@gmail.com>
2019-05-14 11:34:49 +0000

    参加ボタンを押すと表示が切り替わるところを非同期通信にしました

--- Gemfile
+++ Gemfile
@@ -66,3 +66,5 @@ gem 'devise'
 gem "refile", require: "refile/rails", github: 'manfe/refile'
 gem "refile-mini_magick"
 gem 'ransack'
+gem 'jquery-rails'
+gem 'pry-rails'
--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -1,9 +1,9 @@
 class ParticipatesController < ApplicationController
 	def create
-		event = Event.find(params[:event_id])
-		participate = current_user.participates.new(event_id: event.id)
+		@event = Event.find(params[:event_id])
+		participate = current_user.participates.new(event_id: @event.id)
+		@events = Event.all
 		participate.save
-		redirect_back(fallback_location: events_path)
 	end
 
 	def destroy
