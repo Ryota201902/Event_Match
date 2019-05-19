7d915ac0fed6c9923ee7d39abab7ba5a53451b07
Ryota201902 <deraryo327@gmail.com>
2019-05-19 10:47:10 +0000

    マイページの参加ボタンを非同期通信にしました

--- app/controllers/participates_controller.rb
+++ app/controllers/participates_controller.rb
@@ -1,9 +1,10 @@
 class ParticipatesController < ApplicationController
 	def create
 		@event = Event.find(params[:event_id])
-		participate = current_user.participates.new(event_id: @event.id)
+		@participate = current_user.participates.new(event_id: @event.id)
+		@participates = Participate.where(user_id: @participate.user.id)
 		@events = Event.all
-		participate.save
+		@participate.save
 	end
 
 	def destroy
