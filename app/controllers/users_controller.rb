e8d7654d3202c8716cc99bff5aff268af3792033
Ryota201902 <deraryo327@gmail.com>
2019-05-23 01:23:00 +0000

    マイページにページネーション機能を実装しました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
-  	@participates = Participate.where(user_id: @user.id)
+  	@participates = Participate.where(user_id: @user.id).page(params[:page]).reverse_order
   end
 
   def edit
